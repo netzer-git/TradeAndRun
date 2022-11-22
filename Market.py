527b35d18e021a22abed2d49965a7ef57f7888cd
Netzer <netzer.epstein@mail.huji.ac.il>
2022-11-22 15:27:37 +0000

    Arranging market structure

--- Market.py
+++ Market.py
@@ -1,4 +1,27 @@
+import LOB
+
+
 class Market:
 
-    def __init__(self):
+    def __init__(self, length, traders, supply_n_demand):
+        self.traders = traders
+        self.supply_n_demand = supply_n_demand
+        self.current_cycle: int = 1
+        self.last_cycle = self.current_cycle + length
+        self.lob = LOB.LOB()
+
+    def run_market(self):
+        while self.current_cycle <= self.last_cycle:
+            self.run_day()
+            self.current_cycle += 1
+
+    def run_day(self):
+        # cancel expired orders
+
+        # ask trader to order
+
+        # check if order, if so -> process
+
+        # traders response
+
         pass
