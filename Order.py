4236b7fbde496b314088ecfc336a3534b884b0ab
Netzer <netzer.epstein@mail.huji.ac.il>
2022-11-22 15:27:25 +0000

    First fields for classes

--- Order.py
+++ Order.py
@@ -1,4 +1,10 @@
 class Order:
 
-    def __init__(self):
-        pass
+    def __init__(self, oid, tid, otype, price, qnt, start_time, length):
+        self.oid: int = oid
+        self.tid: int = tid
+        self.otype = otype
+        self.price: float = price
+        self.qnt: int = qnt
+        self.start_time: int = start_time
+        self.end_time: int = start_time + length
--- Trader.py
+++ Trader.py
@@ -1,3 +1,15 @@
 class Trader:
-    def __init__(self):
-        pass
+    def __init__(self, tid, ttype, start_time):
+        self.tid: int = tid
+        self.ttype: str = ttype
+        self.balance: float = 0
+        self.start_time: int = start_time
+        self.n_trades: int = 0
+        self.log = []
+        self.orders = []
+
+    def get_order(self):
+        return None
+
+    def respond(self):
+        return None
