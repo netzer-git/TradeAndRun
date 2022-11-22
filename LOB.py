adf09aab71ae135c0979f5474238d721fbe097ea
Netzer <netzer.epstein@mail.huji.ac.il>
2022-11-22 15:27:05 +0000

    Arranging LOB

--- LOB.py
+++ LOB.py
@@ -1,4 +1,23 @@
 class LOB:
 
     def __init__(self):
-        pass
+        self.asks = []
+        self.bids = []
+
+    def add_order(self, order):
+        if order.otype == "bid":
+            self.bids.append(order)
+            self.bids.sort(key=lambda o: o.price)
+        if order.otype == "ask":
+            self.asks.append(order)
+            self.asks.sort(key=lambda o: o.price)
+
+    def get_bids(self):
+        if len(self.bids) == 0:
+            return []
+        return [self.bids[0], self.bids[-1]]
+
+    def get_asks(self):
+        if len(self.asks) == 0:
+            return []
+        return [self.asks[0], self.asks[-1]]
