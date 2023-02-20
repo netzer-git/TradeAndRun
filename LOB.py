f5793e2c86ea8119deee143d50e9973d890c6f46
Netzer <netzer.epstein@mail.huji.ac.il>
2023-02-20 19:24:14 +0200

    Readying the project for actual work

--- LOB.py
+++ LOB.py
@@ -5,27 +5,10 @@ class LOB:
         self.bids = []
 
     def add_order(self, order):
-        if order.otype == "bid":
-            self.bids.append(order)
-            self.bids.sort(key=lambda o: -o.price)
-        if order.otype == "ask":
-            self.asks.append(order)
-            self.asks.sort(key=lambda o: o.price)
+        pass
 
     def remove_from_bids(self, oid):
         pass
 
     def remove_from_asks(self, oid):
         pass
-
-    def get_edge_bids(self):
-        if len(self.bids) == 0:
-            return []
-        # [0] is the cheapest, [-1] is the most expensive
-        return [self.bids[0], self.bids[-1]]
-
-    def get_edge_asks(self):
-        if len(self.asks) == 0:
-            return []
-        # [0] is the cheapest, [-1] is the most expensive
-        return [self.asks[0], self.asks[-1]]
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+# 1. init Market (with cycle num)
+# 2. (in Market) init traders
+# 3. (in Market) init orders
+# 4. Run market for cycle num:
+#   4.1 Roll random trader
+#   4.2 Ask trader for order (give him all the data it needs)
+#   4.3 either:
+#       4.3.1 match new order with existing order (log it to traders, remove from LOB +)
+#       4.3.2 Add to LOB (associate with trader +)
+#   4.4 for t in traders: t.respond()
+#   4.5 (future) remove expired, add new orders
+# 5. close market - publish results and logs
+
+def main():
+    return
+
 if __name__ == "__main__":
-    pass
+    main()
